e40fd66dc1c2f473f61abab4fb7e64b35c1630f8
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2016-02-21 21:16:09 -0500

    New upload way implemented. It's simpler and more correct

--- app/controllers/pictures.server.controller.js
+++ app/controllers/pictures.server.controller.js
@@ -3,25 +3,15 @@ var TempPicture = require('mongoose').model('TempPicture');
 var storage = require('../../lib/StorageProvider/S3StorageProvider');
 
 exports.upload = function(req, res, next) {
-    //Validate that it contains the file name w
-    storage.putObject(req.body)
-        .then(function(data) {
-            var tempParams = {
-                title: req.body.name,
-                album: req.album.id,
-                uploadUrl: data.uploadUrl
-            };
-            var tempPicture = new TempPicture(tempParams);
-            tempPicture.save(function (err) {
-                if (err) {
-                    return next(err);
-                }
-                else {
-                    res.json(data);
-                }
-            });
+    //Validation
+    var file1 = req.file;
 
-        }, function(err) {
-            return next(err);
+    storage.putObject(file1)
+        .then(function(data) {
+            console.log(data);
+            res.json(data);
+            //TODO: Create actual pic model
+        }, function(error) {
+            next(error);
         });
 };
--- app/routes/picture.server.routes.js
+++ app/routes/picture.server.routes.js
@@ -1,11 +1,13 @@
 var pictures = require('../controllers/pictures.server.controller.js');
 var albums = require('../controllers/albums.server.controller.js');
+var multer = require('multer');
+var upload = multer();
 
 module.exports = function(app) {
     app.get('/upload', function(req, res) {
         res.render('upload.html');
     });
     app.route('/albums/:albumId/pictures')
-        .post(pictures.upload);
+        .post(upload.single('picture'), pictures.upload);
     app.param('albumId', albums.albumById);
 };
